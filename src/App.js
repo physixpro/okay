1f4d016a3afb1a2abb2d08f75b5f352b02ccdbda
Keagan <esorts.k990@gmail.com>
2020-10-24 22:18:20 -0400

    lets go

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+import Practice from "./components/Practice";
+
+function App() {
+  return (
+    <div>
+      <Practice />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Practice.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class Practice extends Component{
+    constructor(props){super(); this.state={heading: "Welcome to the new beginning of react" }}
+    render(){
+        return(
+            <div>
+                <h1 className="header" onClick={()=> this.setState({heading:"cool"})}>{this.state.heading}</h1>
+            </div>
+        )
+    }
+}
+
+export default Practice;
